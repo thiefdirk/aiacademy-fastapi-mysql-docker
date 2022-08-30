5a5218bfe7778355d293874ce71f7369c2c85807
thiefdirk <balcony_rarer_0z@icloud.com>
2022-08-30 15:39:56 +0900

    0830

--- app/api/endpoints/url.py
+++ app/api/endpoints/url.py
@@ -2,9 +2,10 @@ from app.services.calculator import CalculatorService
 from app.services.user import UserService
 from app.services.grade import GradeService
 from app.services.pandas_quiz import Pandas_Quiz
+from app.services.ddarung import DDarungService
 import icecream as ic
 from app.constants.menus import LOGIN, LOGOUT, CALCULATOR, GRADE, \
-    QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, QUIZ_5, QUIZ_6, QUIZ_7
+    QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, QUIZ_5, QUIZ_6, QUIZ_7, DDARUNG
 class Url:
     
     def router(self, menu):
@@ -23,6 +24,9 @@ class Url:
             math = int(input('수학'))
             print(f'이름: {name} \
                 학점: {GradeService().get_grade(name,korean, english, math)}')
+        elif menu == DDARUNG: DDarungService().submit(
+            path='data/', train='train.csv', test='test.csv'
+        )
         elif menu == QUIZ_1: Pandas_Quiz().q1()
         elif menu == QUIZ_2: Pandas_Quiz().q2()
         elif menu == QUIZ_3: Pandas_Quiz().q3()
--- app/main.py
+++ app/main.py
@@ -75,15 +75,17 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 basedir = os.path.dirname(os.path.abspath(__file__))
 from app.api.endpoints.url import Url
-from app.constants.menus import LOGIN, LOGOUT, CALCULATOR, GRADE, \
+from app.constants.menus import DDARUNG, LOGIN, LOGOUT, CALCULATOR, GRADE, \
     QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, QUIZ_5, QUIZ_6, QUIZ_7
+
 
 def print_menu():
     print(' ###################')
     print(f'로그인 : {LOGIN}')
     print(f'로그아웃 : {LOGOUT}')
     print(f'계산기 : {CALCULATOR}')
-    print(f'성적표 : {GRADE}') 
+    print(f'성적표 : {GRADE}')
+    print(f'따릉이 : {DDARUNG}') 
     print(f'퀴즈 1 : {QUIZ_1}') 
     print(f'퀴즈 2 : {QUIZ_2}')
     print(f'퀴즈 3 : {QUIZ_3}')
